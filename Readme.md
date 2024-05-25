c2d35eee3c5d597974833867bd3684689c21aeba
Rafaelltm <rafaelltm10@hotmail.com>
2024-05-25 11:11:18 -0300

    adiciona variavel de ambiente ao front end

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# TESTE DE PRÁTICO LUMI – DESENVOLVEDOR(A) FULL STACK PLENO(A)
+###### Rafael Leão Teixeira de Magalhães
--- teste-lumi-front/src/services/api.ts
+++ teste-lumi-front/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+console.log(process.env);
+
+const baseURL = process.env.REACT_APP_API_ADDRESS || "http://localhost:3001";
+
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: baseURL,
   maxContentLength: 10000000,
   maxBodyLength: 10000000,
 });
